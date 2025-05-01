30a472439049b490e9037c07a4865166c3069afd
rufustech <mucherinvest@gmail.com>
2025-05-01 07:26:01 -0600

    fixing deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: [], // Add any external image domains if needed
+      unoptimized: true
+    }
+  }
 
 export default nextConfig;
